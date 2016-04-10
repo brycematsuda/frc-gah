cd6d140710a79498ede29476b30a7fd4e8612492
Bryce Matsuda <brycematsuda@gmail.com>
2016-04-09 17:13:00 -0700

    List team awards for current year.

--- frc-gah.rb
+++ frc-gah.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require_relative 'thebluealliance-ruby/api.rb'
+require 'date'
 
 before do
   @tba = TBA_API.new('brycematsuda', 'frc-gah', '0.0.0')
@@ -40,7 +41,14 @@ helpers do
   def get_alliance_data(alliance)
     alliance_data = []
     alliance.each do |team_key|
-      alliance_data.push(@tba.get_team(team_key))
+      team_data = @tba.get_team(team_key)
+      team_awards = @tba.get_team_history_awards(team_key).select {|award| award['year'] == Date.today.year}
+      awards_data = []
+      team_awards.each do |award|
+        awards_data.push(award['year'].to_s + " " + @tba.get_event(award['event_key'])['name'] + " " + award['name'])
+      end
+      team_data['awards'] = awards_data
+      alliance_data.push(team_data)
     end
 
     return alliance_data
